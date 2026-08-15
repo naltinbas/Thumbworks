e925afde68044f3b8fedfbcf602878623086327a
Necati Altinbas <altinbas@usc.edu>
2026-08-15 08:44:29 +0000

    Pinholt: withdrawn, Crookmarsh already holds the happy ending theorem

--- Pinholt/android/app/src/main/kotlin/com/thumbworks/pinholt/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.thumbworks.pinholt
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Pinholt/lib/main.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'ui/app.dart';
-
-void main() => runApp(const PinholtApp());
